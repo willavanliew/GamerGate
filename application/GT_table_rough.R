2dc8ffff38f4a48120963ad2b44e15ff427b2ef2
Oliver Faulkner Anderson <Olliefanderson@gmail.com>
2023-07-10 20:13:36 -0700

    O: Create gt.csv for use in gt table
    Add 4 tabs to shiny app and add gt.csv for use in shiny

--- application/GT_table_rough.R
+++ application/GT_table_rough.R
@@ -2,118 +2,118 @@ if(!require("pacman")) {install.packages("pacman");library(pacman)}
 p_load(tidyverse, colorspace, RColorBrewer,htmltools,plotly,ggpubr,thematic,shinythemes,gt,stringr)
 
 # Reading in datasets
-games_old <- read_csv("cleaned_games.csv")
-games_new <- read_csv("game_data.csv")
+games <- read_csv("master.csv")
 
-# Create datset for use in Shiny App
-# Select specific columns from the 'games_new' data frame
-games <- games_new %>% 
-  select(name, metacritic, platform_name, platform_released_at, esrb_rating_name)
+# Create dataset for use in gt table
+games_gt <- games %>%
+  select(-c(id, description))
 
-# Select specific columns from the 'games_old' data frame
-games2 <- games_old %>% 
-  select(Title, review_score, release_console, year, esrb_rating)
-
-# Convert the 'year' column in 'games2' to a Date format
-games2$year <- as.Date(paste(games2$year, 1, 1, sep = "-"))
-
-# Rename the column names of 'games2' to match 'games'
-colnames(games2) <- c("name", "metacritic", "platform_name", "platform_released_at", "esrb_rating_name")
-
-# Merge the 'games' and 'games2' data frames vertically
-games <- rbind(games, games2)
-
-# Rename columns to easy variable names
-colnames(games)<-c("title", "score", "platform", "date", "esrb")
-
-# Change ESRB rating to consistent naming convention and correct X360 platform name
-games <- games %>% mutate(esrb = case_when(
-  esrb == "Mature" ~ "Mature",
-  esrb == "Everyone 10+" ~ "Everyone 10+",
-  esrb == "Everyone" ~ "Everyone",
-  esrb == "Teen" ~ "Teen",
-  esrb == "Adults Only" ~ "Adults Only",
-  esrb == "Rating Pending" ~ "Rating Pending",
-  esrb == "M" ~ "Mature",
-  esrb == "E" ~ "Everyone",
-  esrb == "T" ~ "Teen")
-  )
-
-# replace names to consistent naming convention
-games$platform <- str_replace(games$platform, 'X360', 'Xbox 360')
-games$platform <- str_replace(games$platform, 'Wii', 'Nintendo Wii')
-
-# Add platform logos by URL
-games <- games %>% 
+# Add platform_name logos by URL
+games_gt <- games_gt %>% 
   mutate(platform_logo = 
          case_when(
-           platform == "PlayStation 5" ~ "https://upload.wikimedia.org/wikipedia/commons/c/cb/PlayStation_5_logo_and_wordmark.svg",
-           platform == "PlayStation 4" ~ "https://upload.wikimedia.org/wikipedia/commons/8/87/PlayStation_4_logo_and_wordmark.svg",
-           platform == "Xbox Series S/X" ~ "https://upload.wikimedia.org/wikipedia/commons/f/f3/Xbox_Series_X_S_black.svg",
-           platform == "PC" ~ "https://upload.wikimedia.org/wikipedia/commons/3/38/Pc_game_logo.png",
-           platform == "PlayStation 3" ~ "https://upload.wikimedia.org/wikipedia/commons/0/05/PlayStation_3_logo_%282009%29.svg",
-           platform == "Xbox 360" ~ "https://upload.wikimedia.org/wikipedia/en/0/0c/Xbox_360_full_logo.svg",
-           platform == "Xbox One" ~ "https://upload.wikimedia.org/wikipedia/commons/4/43/Xbox_One_logo_wordmark.svg",
-           platform == "Nintendo Switch" ~ "https://upload.wikimedia.org/wikipedia/commons/8/8a/NintendoSwitchLogo.svg",
-           platform == "Linux" ~ "https://upload.wikimedia.org/wikipedia/commons/d/dd/Linux_logo.jpg",
-           platform == "macOS" ~ "https://upload.wikimedia.org/wikipedia/commons/3/30/MacOS_logo.svg",
-           platform == "Android" ~ "https://upload.wikimedia.org/wikipedia/commons/6/64/Android_logo_2019_%28stacked%29.svg",
-           platform == "PS Vita" ~ "https://upload.wikimedia.org/wikipedia/commons/3/3d/PlayStation_Vita_logo.svg",
-           platform == "iOS" ~ "https://upload.wikimedia.org/wikipedia/commons/6/63/IOS_wordmark_%282017%29.svg",
-           platform == "Xbox" ~ "https://upload.wikimedia.org/wikipedia/commons/8/8c/XBOX_logo_2012.svg",
-           platform == "Web" ~ "https://upload.wikimedia.org/wikipedia/commons/c/c4/Globe_icon.svg",
-           platform == "Wii U" ~ "https://upload.wikimedia.org/wikipedia/commons/7/7e/WiiU.svg",
-           platform == "Nintendo 3DS" ~ "https://upload.wikimedia.org/wikipedia/commons/1/19/Nintendo_3ds_logo.svg",
-           platform == "PlayStation 2" ~ "https://upload.wikimedia.org/wikipedia/commons/7/76/PlayStation_2_logo.svg",
-           platform == "Dreamcast" ~ "https://upload.wikimedia.org/wikipedia/commons/7/7e/Dreamcast_logo.svg",
-           platform == "Classic Macintosh" ~ "https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg",
-           platform == "Nintendo DS" ~ "https://upload.wikimedia.org/wikipedia/commons/a/af/Nintendo_DS_Logo.svg",
-           platform == "GameCube" ~ "https://upload.wikimedia.org/wikipedia/en/e/e6/Nintendo_Gamecube_Logo.svg",
-           platform == "Nintendo 64" ~ "https://upload.wikimedia.org/wikipedia/en/0/04/Nintendo_64_Logo.svg",
-           platform == "PlayStation" ~ "https://upload.wikimedia.org/wikipedia/commons/4/4e/Playstation_logo_colour.svg",
-           platform == "Game Boy Advance" ~ "https://upload.wikimedia.org/wikipedia/commons/8/8a/Gameboy_advance_logo.svg",
-           platform == "Sony PSP" ~ "https://upload.wikimedia.org/wikipedia/commons/0/0e/PSP_Logo.svg",
-           platform == "Nintendo Wii" ~ "https://upload.wikimedia.org/wikipedia/commons/b/bc/Wii.svg"
+           platform_name == "PlayStation 5" ~ "https://upload.wikimedia.org/wikipedia/commons/c/cb/PlayStation_5_logo_and_wordmark.svg",
+           platform_name == "PlayStation 4" ~ "https://upload.wikimedia.org/wikipedia/commons/8/87/PlayStation_4_logo_and_wordmark.svg",
+           platform_name == "Xbox Series S/X" ~ "https://upload.wikimedia.org/wikipedia/commons/f/f3/Xbox_Series_X_S_black.svg",
+           platform_name == "PC" ~ "https://upload.wikimedia.org/wikipedia/commons/3/38/Pc_game_logo.png",
+           platform_name == "PlayStation 3" ~ "https://upload.wikimedia.org/wikipedia/commons/0/05/PlayStation_3_logo_%282009%29.svg",
+           platform_name == "Xbox 360" ~ "https://upload.wikimedia.org/wikipedia/en/0/0c/Xbox_360_full_logo.svg",
+           platform_name == "Xbox One" ~ "https://upload.wikimedia.org/wikipedia/commons/4/43/Xbox_One_logo_wordmark.svg",
+           platform_name == "Nintendo Switch" ~ "https://upload.wikimedia.org/wikipedia/commons/8/8a/NintendoSwitchLogo.svg",
+           platform_name == "Linux" ~ "https://upload.wikimedia.org/wikipedia/commons/d/dd/Linux_logo.jpg",
+           platform_name == "macOS" ~ "https://upload.wikimedia.org/wikipedia/commons/3/30/MacOS_logo.svg",
+           platform_name == "Android" ~ "https://upload.wikimedia.org/wikipedia/commons/6/64/Android_logo_2019_%28stacked%29.svg",
+           platform_name == "PS Vita" ~ "https://upload.wikimedia.org/wikipedia/commons/3/3d/PlayStation_Vita_logo.svg",
+           platform_name == "iOS" ~ "https://upload.wikimedia.org/wikipedia/commons/6/63/IOS_wordmark_%282017%29.svg",
+           platform_name == "Xbox" ~ "https://upload.wikimedia.org/wikipedia/commons/8/8c/XBOX_logo_2012.svg",
+           platform_name == "Web" ~ "https://upload.wikimedia.org/wikipedia/commons/c/c4/Globe_icon.svg",
+           platform_name == "Wii U" ~ "https://upload.wikimedia.org/wikipedia/commons/7/7e/WiiU.svg",
+           platform_name == "Nintendo 3DS" ~ "https://upload.wikimedia.org/wikipedia/commons/1/19/Nintendo_3ds_logo.svg",
+           platform_name == "PlayStation 2" ~ "https://upload.wikimedia.org/wikipedia/commons/7/76/PlayStation_2_logo.svg",
+           platform_name == "Dreamcast" ~ "https://upload.wikimedia.org/wikipedia/commons/7/7e/Dreamcast_logo.svg",
+           platform_name == "Classic Macintosh" ~ "https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg",
+           platform_name == "Nintendo DS" ~ "https://upload.wikimedia.org/wikipedia/commons/a/af/Nintendo_DS_Logo.svg",
+           platform_name == "GameCube" ~ "https://upload.wikimedia.org/wikipedia/en/e/e6/Nintendo_Gamecube_Logo.svg",
+           platform_name == "Nintendo 64" ~ "https://upload.wikimedia.org/wikipedia/en/0/04/Nintendo_64_Logo.svg",
+           platform_name == "PlayStation" ~ "https://upload.wikimedia.org/wikipedia/commons/4/4e/Playstation_logo_colour.svg",
+           platform_name == "Game Boy Advance" ~ "https://upload.wikimedia.org/wikipedia/commons/8/8a/Gameboy_advance_logo.svg",
+           platform_name == "Sony PSP" ~ "https://upload.wikimedia.org/wikipedia/commons/0/0e/PSP_Logo.svg",
+           platform_name == "Wii" ~ "https://upload.wikimedia.org/wikipedia/commons/b/bc/Wii.svg",
+           platform_name == "3DO" ~ "https://upload.wikimedia.org/wikipedia/en/4/4d/3DO_Interactive_Multiplayer_logo.png",
+           platform_name == "Game Boy Color" ~ "https://upload.wikimedia.org/wikipedia/commons/c/c5/Game_Boy_Color_logo.svg",
+           platform_name == "SNES" ~ "https://upload.wikimedia.org/wikipedia/commons/3/33/Super_Nintendo_Entertainment_System_logo.svg",
+           platform_name == "SEGA Saturn" ~ "https://upload.wikimedia.org/wikipedia/en/4/42/SegaSaturn.png",
+           platform_name == "SEGA 32X" ~ "https://upload.wikimedia.org/wikipedia/commons/a/a4/Sega_32X_logo.svg",
+           platform_name == "Jaguar" ~ "https://upload.wikimedia.org/wikipedia/en/d/dc/Jaguar_Logo.png",
+           platform_name == "Genesis" ~ "https://upload.wikimedia.org/wikipedia/en/1/12/GenesisLogo.png",
+           platform_name == "Game Boy" ~ "https://upload.wikimedia.org/wikipedia/commons/f/f2/Nintendo_Game_Boy_Logo.svg",
+           platform_name == "NES" ~ "https://upload.wikimedia.org/wikipedia/commons/0/0d/NES_logo.svg",
+           platform_name == "Commodore / Amiga" ~ "https://upload.wikimedia.org/wikipedia/commons/c/cc/Commodore_Amiga_logo-03.svg",
+           platform_name == "Game Gear" ~ "https://upload.wikimedia.org/wikipedia/commons/b/b8/Game_Gear_logo_Sega.png",
+           platform_name == "Nintendo DSi" ~ "https://upload.wikimedia.org/wikipedia/commons/d/dd/Nintendo_DSi_logo.svg",
+           platform_name == "SEGA Master System" ~ "https://upload.wikimedia.org/wikipedia/commons/3/3e/Sega-master-system-logo.png"
          )
-)
+) %>% 
+  mutate(cons_comp = 
+           case_when(
+             platform_name %in% c("PlayStation 5", "PlayStation 4", "PlayStation 3", "PS Vita",
+                                  "PlayStation 2", "PlayStation", "Sony PSP") ~ "Sony",
+             platform_name %in% c("Xbox Series S/X", "Xbox 360", "Xbox One", "Xbox") ~ "Microsoft",
+             platform_name %in% c("Nintendo Switch", "Wii U", "Nintendo 3DS", "Nintendo DS",
+                                  "GameCube", "Nintendo 64", "Wii", "Game Boy Advance",
+                                  "Game Boy Color", "SNES", "Game Boy", "NES",
+                                  "Nintendo DSi") ~ "Nintendo",
+             platform_name %in% c("Dreamcast", "SEGA Saturn", "SEGA 32X",
+                                  "Genesis", "Game Gear", "SEGA Master System") ~ "SEGA",
+             platform_name %in% c("3DO", "Jaguar", "Commodore / Amiga") ~ "Other",
+             platform_name %in% c("Android", "iOS") ~ "Mobile",
+             platform_name %in% c("PC", "Linux", "macOS", "Web", "Classic Macintosh") ~ "Computer"
+             )
+  )
 
+head(games_gt)
 
-# create data set without NA values for some functions
-noNA_games <- na.omit(games)
+# write to csv
+# write_csv(games_gt, "gt.csv")
 
 # create initial GT table object
-gt_games <- games %>%
+games_table <- games_gt %>%
   gt() %>% 
   # add title and subtitle
   tab_header(
     title = md("**GamerGate 2023 Games**"),
     subtitle = "Explore the data used to train the algorithm"
   ) %>% 
-  # format date
-  fmt_date(
-    columns = date,
-    date_style = 5
-  ) %>% 
-  # color score column based on values
+  # color metacritic column based on values
   data_color(
-    columns = score,
+    columns = metacritic,
     fn = scales::col_bin(
       palette = "PuBu",
-      domain = range(noNA_games$score),
+      domain = range(games$metacritic),
       bins = c(0, 25, 50, 70, 80, 90, 100)
     ),
     alpha = 0.9
   ) %>% 
   # add official column names
   cols_label(
-    title = md("**Game Title**"),
-    score = md("**Metacritic Rating**"),
-    platform = "Gaming Platforms",
-    date = "Release Date",
-    esrb = "ESRB Rating"
+    name = md("**Game Title**"),
+    metacritic = md("**Metacritic Rating**"),
+    platform_name = "Gaming Platforms",
+    esrb_rating_name = "ESRB Rating",
+    playtime = "Game Duration (mins)",
+    background_image = "Background Image",
+    genre = "Genre",
+    metacritic_category = "Metacritic Category",
+    season = "Time of Release",
+    platform_logo = "Platform Logo"
   ) %>% 
+  # put metacritic columns next to each other
+  cols_move(metacritic_category, metacritic) %>%
+  # put platform logo next to platform name
+  cols_move(platform_logo, platform_name) %>% 
+  # Add images to GT table
   text_transform(
-    # Apply a function to a column
+    # Apply a function to a column to return the platform logo
     locations = cells_body(platform_logo),
          fn = function(x) {
            #Return an image of set dimensions
@@ -122,13 +122,24 @@ gt_games <- games %>%
              height = 12
            )
          }
+       ) %>% 
+       text_transform(
+    # Apply a function to a column to return the game logo
+    locations = cells_body(background_image),
+         fn = function(x) {
+           # Return an image of set dimensions
+           web_image(
+             url = x,
+             height = 12
+           )
+         }
        )
 
 # display the table
-gt_games
+games_table
 
 # add interactive search function to table
-opt_interactive(gt_games,
+opt_interactive(games_table,
     use_search = TRUE,
     use_page_size_select = TRUE
 )
