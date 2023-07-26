b2ce9b944c7949282bd98be1eb063559c068b759
willavanliew <wgvanliew@willamette.edu>
2023-07-25 18:28:28 -0700

    W:Start of reactive function

--- application/WillasApp/app.R
+++ application/WillasApp/app.R
@@ -1,6 +1,10 @@
 if(!require("pacman")) {install.packages("pacman");library(pacman)}
 p_load(tidyverse, colorspace, RColorBrewer,htmltools,plotly,ggpubr,thematic,shinythemes,shiny)
-
+games <- import("application/Final_dev_pub.csv")
+dev_names <- import("application/developer_titles.csv")
+pub_names <- import("application/publisher_tites.csv")
+platform_names <- games %>% select(c(91:136)) %>% names()
+genres_names <- games %>% select(c(140:145)) %>% names() 
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -11,19 +15,21 @@ ui <- fluidPage(
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-          selectizeInput("games", "Choose up to 5 Games:", unique(games$name), 
-                         multiple = TRUE, 
-                         selected = "Grand",
-                         options = list(maxItems = 3))
-          selectInput("input", h3("Choose value"), 
-                      choices = list("Genre" = genre,
-                                     "Console Company" = cons_comp,
-                                     "Console Type" = platform_name,
-                                     "Metacritic Score" = metacritic_category,
-                                     "Publisher" = publisher,
-                                     "Developer" = developer,
-                                     "ESRB Rating" = esrb_rating_name), selected = genre),
-          
+          selectInput("genre", h3("Choose a genre"), 
+                      choices = genres_names,
+                      selected = "All"),
+          selectInput("console", h3("Choose a Console Type"),
+                      choices = platform_names,
+                      select = "All"),
+          selectInput("publisher", h3("Choose a Publisher"),
+                      choices = list(pub_names$Name), # selection -> filter on input$publisher, select column 
+                      select = "All"),
+          selectInput("developer", h3("Choose a Developer"),
+                      choices = list(dev_names$Name),
+                      select = "All"),
+          selectInput("esrb", h3("Choose a ESRB Rating"),
+                      choices = list(unique(games$esrb_rating_name)),
+                      select = "All")
         ),
 
         # Show a plot of the generated distribution
@@ -33,6 +39,12 @@ ui <- fluidPage(
     )
 )
 
+games_reactive <- reactive(
+  # Make it so it only selects what we want to select 
+  
+  games %>%
+    
+)
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
