8d4d183fdb890059b79264002c665f3b8ed07551
Oliver Faulkner Anderson <Olliefanderson@gmail.com>
2023-07-28 19:02:06 -0700

    O: added basic search functions
    Added search functions dependent on column names, can be converted to reactive functions easily

--- application/WillasApp/app.R
+++ application/WillasApp/app.R
@@ -20,11 +20,100 @@ mode_index <- 132:134
 # index of genre
 genre_index <- 135:140
 
-# write to csv
-write_csv(games_gt, "gt.csv")
+
+
+### PLAYING AROUND WITH FUNCTIONS
+# A function that works to search the developer columns
+search_dev <- function(x) {
+  dev_index <- 6:40  # Definition of dev_index
+  # add dev_ to user input
+  x <- paste0("dev_", x)
+  # Check if x matches any of the modified column names
+  if (x %in% colnames(games_gt[, dev_index])) {
+    result <- games_gt[games_gt[, dev_index][, x] == "Yes", ]
+    return(result)
+  } else {
+    cat("Developer not found:", x, "\n")
+    return(NULL)
+  }
+}
+
+search_dev("Atlus")
+
+# A function that works to search the publisher columns
+search_pub <- function(x) {
+  # Get the index of columns starting with "pub_"
+  pub_index <- 41:85
+  # add dev_ to user input
+  x <- paste0("pub_", x)
+  # Check if x matches any of the modified column names
+  if (x %in% colnames(games_gt[, pub_index])) {
+    result <- games_gt[games_gt[, pub_index][, x] == "Yes", ]
+    return(result)
+  } else {
+    cat("Publisher not found:", x, "\n")
+    return(NULL)
+  }
+}
+
+search_pub("Atlus")
+
+# A function that works to search the platform columns
+search_platform <- function(x) {
+  # index of platforms
+  platform_index <- 86:131
+
+  # Check if x matches any of the modified column names
+  if (x %in% colnames(games_gt[, platform_index])) {
+    result <- games_gt[games_gt[, platform_index][, x] == "Yes", ]
+    return(result)
+  } else {
+    cat("Platform not found:", x, "\n")
+    return(NULL)
+  }
+}
+
+search_platform("PlayStation 5") # PS5 does not work but maybe we can add another column for it if we desire
+# NOTE: Do we want to add in more dummies for PS5, PS4 as well as PlayStation 5 since people may search that?
+# Consider: converting to all caps in reactive func to make search case insensitive
+
+# A function that works to search the gaming mode columns
+search_mode <- function(x) {
+  # index of modes
+  mode_index <- 132:134
+  
+  # Check if x matches any of the modified column names
+  if (x %in% colnames(games_gt[, mode_index])) {
+    result <- games_gt[games_gt[, mode_index][, x] == "Yes", ]
+    return(result)
+  } else {
+    cat("Mode not found:", x, "\n")
+    return(NULL)
+  }
+}
+
+search_mode("2-player co-op")
+# in reactive function we definitely want a select drop down for gaming mode, not a search func
+
+# A function that works to search the gaming mode columns
+search_genre <- function(x) {
+  # index of genre
+  genre_index <- 135:140
+  
+  # Check if x matches any of the modified column names
+  if (x %in% colnames(games_gt[, genre_index])) {
+    result <- games_gt[games_gt[, genre_index][, x] == "Yes", ]
+    return(result)
+  } else {
+    cat("Genre not found:", x, "\n")
+    return(NULL)
+  }
+}
+
+search_genre("Action")
 
 # create initial GT table object
-games_table <- games_gt %>%
+games_table <- games_gt %>% # replace this with reactive dataset that is formed from search func
   # drop the unnecessary columns
   select(-(6:140)) %>%
   # arrange always by metacritic rating first
